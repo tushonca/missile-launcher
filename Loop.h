4c8111a130b81a585d17d74a67d84a427d5d3ced
tushonca <ilya_f_20082008@list.ru>
2025-05-16 14:24:39 +0500

    Delete Loop.h

--- Loop.h
+++
@@ -1,8 +0,0 @@
-void loop() {
-  static byte mode = 0;
-  static unsigned long startTime = 0;
-  static unsigned long start1EngineTime = 0;
-  static unsigned long start2EngineTime = 0;
-  sBus.FeedLine();
-
-}
