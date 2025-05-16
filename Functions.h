33a07d096711fc0716f75c4b4d8474fb260ed7d3
tushonca <ilya_f_20082008@list.ru>
2025-05-16 14:24:13 +0500

    Delete Functions.h

--- Functions.h
+++
@@ -1,3 +0,0 @@
-ISR(TIMER1_COMPA_vect) {PINE |= (1 << PINE5);}
-void buzzerTone(uint16_t FREQIN) {OCR1A = FREQIN; TIMSK1 |= (1 << OCIE1A);}
-void buzzerNoTone() {TIMSK1 &= 0b11111101; PORTE  &= 0b11011111; DDRE   &= 0b11011111;}
